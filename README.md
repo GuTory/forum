63a9a20042a22efca71b1c97e9adc109d761ac67
Tóth Kristóf <tothkristof31@edu.bme.hu>
2022-12-02 21:57:59 +0100

    readme update és fölös importok kimetszése

--- README.md
+++ README.md
@@ -8,6 +8,10 @@
 
 MySQL adatbázist használok a 3306-os default porton, `mydb` adatbázisnévvel, azon belül a táblák generáltak. adatbázis paraméterek a db.properties fájlban
 
+## Indításkor
+
+Services alkalmazásban futnia kell a MySQL szervernek
+
 ## Swagger hozzádva a projekthez
 
 http://localhost:8080/swagger-ui/
--- src/main/java/com/temalab/forum/ForumApplication.java
+++ src/main/java/com/temalab/forum/ForumApplication.java
@@ -1,32 +1,10 @@
 package com.temalab.forum;
 
-import com.temalab.forum.model.Category;
-import com.temalab.forum.model.Comment;
-import com.temalab.forum.model.Topic;
-import com.temalab.forum.model.User;
-import com.temalab.forum.repository.CategoryRepository;
-import com.temalab.forum.repository.CommentRepository;
-import com.temalab.forum.repository.TopicRepository;
-import com.temalab.forum.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ForumApplication {
-
-	@Autowired
-	private CategoryRepository categoryRepository;
-
-	@Autowired
-	private CommentRepository commentRepository;
-
-	@Autowired
-	private UserRepository userRepository;
-
-	@Autowired
-	private TopicRepository topicRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
